b1d30a355c424349875d57f0e826e5c85c5f5811
Kiran Anand <liteninkiran@hotmail.com>
2021-08-24 11:29:46 +0100

    Service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,51 +1,9 @@
 'use strict';
 
-eventsApp.controller('EventController', function EventController($scope) {
+eventsApp.controller('EventController', function EventController($scope, eventData) {
 
-    $scope.snippet = '<span style="color:red;">Hi There</span>';
-    $scope.boolValue = false;
-    $scope.mystyle = { color: 'red' };
-    $scope.myclass = 'title-color';
-    $scope.buttonDisabled = false;
-    $scope.buttonText = 'Click to Disable Me';
     $scope.sortOrder = '-upVoteCount';
-    $scope.event = {
-        name: 'Angular Boot Camp',
-        date: 1359781015626,
-        time: '10:30 AM',
-        location: {
-            address: 'Google Headquarters',
-            city: 'Mountain View',
-            province: 'CA',
-        },
-        imageUrl: '/img/angularjs-logo.png',
-        sessions: [
-            {
-                name: 'Directives Masterclass',
-                creatorName: 'Bob Smith',
-                duration: 1,
-                level: 'Advanced',
-                abstract: 'In this session, you will learn the ins and outs of directives.',
-                upVoteCount: 0,
-            },
-            {
-                name: 'Scopes for Fun & Profit',
-                creatorName: "John Doe",
-                duration: 2,
-                level: "Introductory",
-                abstract: "This session will take a closer look at scopes.  Learn what they do, how they do it, and how to get them to do it for you.",
-                upVoteCount: 0,
-            },
-            {
-                name: 'Well Behaved Controllers',
-                creatorName: "Jane Doe",
-                duration: 4,
-                level: "Intermediate",
-                abstract: "Controllers are the beginning of everything Angular does.  Learn how to craft controllers that will win the respect of your friends and neighbors.",
-                upVoteCount: 0,
-            },
-        ],
-    };
+    $scope.event = eventData.event;
 
     $scope.upVoteSession = function(session) {
         session.upVoteCount++;
