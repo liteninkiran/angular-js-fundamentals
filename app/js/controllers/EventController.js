c47d64240ada034565a189b1d7bee8219d0dbc3e
Kiran Anand <liteninkiran@hotmail.com>
2021-08-24 17:09:14 +0100

    Using $resource

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,13 +3,11 @@
 eventsApp.controller('EventController', function EventController($scope, eventData, $log) {
 
     $scope.sortOrder = '-upVoteCount';
+    eventData.getEvent()
+        .$promise
+        .then(function(event) { $scope.event = event; })
+        .catch(function(response) { console.log(response); });
 
-    eventData.getEvent().success(function(event) {
-        $scope.event= event;
-    }).error(function(data, status, headers, config) {
-        $log.warn(data, status, headers, config);
-    });
-        
     $scope.upVoteSession = function(session) {
         session.upVoteCount++;
     };
